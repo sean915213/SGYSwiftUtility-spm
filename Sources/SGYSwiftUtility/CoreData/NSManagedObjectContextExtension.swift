44efa5030069276810946503bd91c6ada4336845
Sean Young <sean915213@users.noreply.github.com>
2021-04-16 18:05:50 -0400

    Removed fetch extension causing ambiguity.

--- Sources/SGYSwiftUtility/CoreData/NSManagedObjectContextExtension.swift
+++ Sources/SGYSwiftUtility/CoreData/NSManagedObjectContextExtension.swift
@@ -14,10 +14,4 @@ extension NSManagedObjectContext {
         // Force cast since object not found will throw and if an object is found of the wrong type the caller messed up.
         return try existingObject(with: objectID) as! T
     }
-    
-    public func fetch<T: NSManagedObject>(_ request: NSFetchRequest<T>) throws -> T? {
-        // We only expect one entry so limit request
-        request.fetchLimit = 1
-        return try fetch(request).first
-    }
 }
