40e9d2a9a39b41d70e411c30ca6e5e124eb2c647
Sean Young <sean915213@users.noreply.github.com>
2021-05-01 13:24:52 -0400

    Improved UserDefaults extension by allowing RawRepresentable storage

---
+++ Sources/SGYSwiftUtility/UserDefaultsExtension.swift
@@ -0,0 +1,35 @@
+//
+//  UserDefaultsExtension.swift
+//  Pods
+//
+//  Created by Sean G Young on 2/13/16.
+//
+//
+
+import Foundation
+
+extension UserDefaults {
+    
+    /// Provides a method for retrieval and casting of UserDefaults values to type `T`.
+    /// - Parameter key: The key for the object in UserDefaults.
+    /// - Returns: An instance of `T` retrieved from this UserDefaults instance or `nil` if no object was found or could not be cast to `T`.
+    public func object<T>(forKey key: String) -> T? {
+        return object(forKey: key) as? T
+    }
+    
+    /// Provides a method for retrieval and casting of UserDefaults values to a RawRepresentable type `T`.
+    /// - Parameter key: The key for the object in UserDefaults.
+    /// - Returns: An instance of `T` initialized from the raw value or `nil` if no value exists or cannot be used to initialize `T`.
+    public func object<T>(forKey key: String) -> T? where T: RawRepresentable {
+        if let rawValue: T.RawValue = object(forKey: key) { return T(rawValue: rawValue) }
+        return nil
+    }
+    
+    /// Stores a RawRepresentable value in UserDefaults.
+    /// - Parameters:
+    ///   - value: The RawRepresentable conforming value.
+    ///   - defaultName: The key with which to associate the value.
+    public func set<T>(_ value: T?, forKey defaultName: String) where T: RawRepresentable {
+        set(value?.rawValue, forKey: defaultName)
+    }
+}
