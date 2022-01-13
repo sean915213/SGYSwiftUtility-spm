9699ab024b1b39a9ab3251b2e83f29c9456c02ef
Sean Young <sean915213@users.noreply.github.com>
2022-01-13 17:13:21 -0500

    Added sheet presentation extension

--- Sources/SGYSwiftUtility/UIKit/UIViewControllerExtension.swift
+++ Sources/SGYSwiftUtility/UIKit/UIViewControllerExtension.swift
@@ -22,6 +22,19 @@ extension UIViewController {
         removeBlock()
         removeFromParent()
     }
+    
+    @available(iOS 15, *)
+    /// Configures a view controller's `UISheetPresentationController` with common values for sheet presentation. Also adds additional logic like increasing safe area insets when grabber is visible.
+    /// - Parameters:
+    ///   - detents: The `Detent`s or sizes to allow the sheet to expand to.
+    ///   - preferGrabberVisible: Whether to prefer displaying the grabber at the top of the controller. If true, the top safe area insets will be increased to account for the grabber.
+    public func configureForSheetPresentation(withDetents detents: [UISheetPresentationController.Detent] = [.medium(), .large()], preferGrabberVisible: Bool = true) {
+        guard let sheet = sheetPresentationController else { return }
+        sheet.detents = detents
+        sheet.prefersGrabberVisible = preferGrabberVisible
+        // If grabber visible then increase the safe area
+        if preferGrabberVisible { additionalSafeAreaInsets.top = 8 }
+    }
 }
 
 #endif
