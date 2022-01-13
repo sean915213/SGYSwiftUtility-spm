b2f1be4b255f719f081a25957841405b74c72d84
Sean Young <sean915213@users.noreply.github.com>
2022-01-13 11:35:54 -0500

    Added UITextFieldDelegate extension

---
+++ Sources/SGYSwiftUtility/UIKit/UITextFieldDelegateExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UITextFieldDelegateExtension.swift
+//  
+//
+//  Created by Sean Young on 1/13/22.
+//
+
+import UIKit
+
+extension UITextFieldDelegate {
+    
+    public func makeNewString(from textField: UITextField, replacementRange range: NSRange, replacementString string: String) -> String {
+        return (textField.text ?? "").replacingCharacters(in: Range<String.Index>(range, in: textField.text ?? "")!, with: string)
+    }
+}
