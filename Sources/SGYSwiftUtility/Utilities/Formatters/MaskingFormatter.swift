e7e818fa80a5f35ec4b96bdb27e72ef96b3b44e5
Sean Young <sean915213@users.noreply.github.com>
2022-01-14 15:41:07 -0500

    Reorganization and addition of MaskingFormatter

---
+++ Sources/SGYSwiftUtility/Utilities/Formatters/MaskingFormatter.swift
@@ -0,0 +1,49 @@
+//
+//  MaskingFormatter.swift
+//  
+//
+//  Created by Sean Young on 1/14/22.
+//
+
+import Foundation
+
+/// Formats a string into a masked version.
+public class MaskingFormatter: Formatter {
+    
+    // MARK: - Properties
+    
+    /// The string that acts as the mask for a masked character.
+    public var maskingCharacter = Character("*")
+    /// The set of characters that should be masked.
+    public var maskedCharacters = CharacterSet()
+    /// The maximum number of characters to mask. If `nil`all characters will be masked.
+    public var maximumMaskCount: Int?
+    
+    // MARK: - Methods
+    
+    override func string(for obj: Any?) -> String? {
+        // Require string to format
+        guard let stringVal = obj as? String else { return nil }
+        // Add masking character to set so we also skip
+        // Create masked version
+        var masked = ""
+        var maskedCount = 0
+        for nextChar in stringVal {
+            // If not in masked characters AND isn't the masking character itself then skip altogether
+            guard maskedCharacters.contains(nextChar) || nextChar == maskingCharacter else {
+                masked.append(nextChar)
+                continue
+            }
+            // If didn't reach masked count then mask
+            if maximumMaskCount == nil || maskedCount < maximumMaskCount! {
+                masked.append(maskingCharacter)
+                maskedCount += 1
+            } else {
+                // Unmasked value
+                masked.append(nextChar)
+            }
+        }
+        // Return masked string
+        return masked
+    }
+}
