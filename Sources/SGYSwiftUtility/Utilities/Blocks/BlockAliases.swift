aa2665a9b7113a7e6ac0efc49ea714bd3c9f1f29
Sean Young <sean915213@users.noreply.github.com>
2022-02-12 13:54:50 -0500

    Added block type aliases

---
+++ Sources/SGYSwiftUtility/Utilities/Blocks/BlockAliases.swift
@@ -0,0 +1,12 @@
+//
+//  BlockAliases.swift
+//  
+//
+//  Created by Sean Young on 2/12/22.
+//
+
+import Foundation
+
+public typealias VoidBlock<Out> = () -> Out
+
+public typealias Param1Block<Param1, Out> = (Param1) -> Out
