a380ec6b06052701e94aa0f33129dc3cef4dfab4
Sean Young <sean915213@users.noreply.github.com>
2022-02-18 15:26:16 -0500

    More regular expression extensions

--- Sources/SGYSwiftUtility/Foundation/NSRegularExpressionExtension.swift
+++ Sources/SGYSwiftUtility/Foundation/NSRegularExpressionExtension.swift
@@ -9,6 +9,38 @@ import Foundation
 
 extension NSRegularExpression {
     
+    /// Possible numeric signs.
+    public enum NumericSign { case none, negative, both }
+    
+    /// Creates a regular expression suitable for validating numeric input.
+    /// - Parameters:
+    ///   - sign: The type of `NumericSign` cases allowed.
+    ///   - fractionalDigits: The number of valid fractional digits.
+    /// - Returns: A regular expression.
+    public static func numericValidation(forSign sign: NumericSign, fractionalDigits: Int?) -> NSRegularExpression {
+        // Construct sign portion of regex
+        let signRegex: String
+        switch sign {
+        case .none:
+            // Unsigned means always positive, so sign portion is blank
+            signRegex = ""
+        case .negative:
+            // Sign must be negative so require
+            signRegex = "-"
+        case .both:
+            // Can be either, so make negative optional
+            signRegex = "-?"
+        }
+        // If no digits use regex not allowing separator, either.
+        let pattern: String
+        if fractionalDigits == 0 {
+            pattern = "^\(signRegex)\\d*$"
+        } else {
+            pattern = "^\(signRegex)\\d*\\\(NSLocale.current.decimalSeparator ?? ".")?\\d{0,\(fractionalDigits ?? 100)}$"
+        }
+        return try! NSRegularExpression(pattern: pattern, options: [])
+    }
+    
     /// Finds the first match within the entire string.
     /// - Parameters:
     ///   - string: The string to match against.
