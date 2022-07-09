d721ef3b616ab3b589273e1c71a74ba36c628733
Sean Young <sean915213@users.noreply.github.com>
2022-07-09 17:38:18 -0400

    Documented ScrollableInputViewController.

--- Sources/SGYSwiftUtility/Protocols/ScrollableInputViewController.swift
+++ Sources/SGYSwiftUtility/Protocols/ScrollableInputViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 
 public protocol ScrollableInputViewController: ScrollableViewController & ScrollViewInputHandler { }
 
+/// Combines `ScrollableViewController` and `ScrollViewInputHandler`.
 extension ScrollableInputViewController {
-    
+    /// Sets up the controller's main view with a scroll view which contains a content view to place content on that can be scrolled in the specified direction. Optionally also registers keyboard observers for modifying offset when keyboard is displayed.
+    /// - Parameters:
+    ///   - scrollableAxis: The direction to allow scrolling in.
+    ///   - registerObservers: Whether to register keyboard observers in order to modify content when keyboard is shown. Defaults to `true`.
     public func setupContentView(withScrollableAxis scrollableAxis: NSLayoutConstraint.Axis, registerObservers: Bool = true) {
         (self as ScrollableViewController).setupContentView(withScrollableAxis: scrollableAxis)
         if registerObservers { registerKeyboardObservers() }
     }
-    
 }
 
 #endif
