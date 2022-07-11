2c2128709bde5d285a32b55758b61e82f4568c78
Sean Young <sean915213@users.noreply.github.com>
2022-07-11 14:37:02 -0400

    PropertyListManager enhancements

--- Sources/SGYSwiftUtility/Foundation/Utility/PropertyListManager.swift
+++ Sources/SGYSwiftUtility/Foundation/Utility/PropertyListManager.swift
@@ -11,42 +11,57 @@ private let plistCache = NSCache<NSString, ValueWrapper>()
 private let decoder = PropertyListDecoder()
 
 extension PropertyListManager.Name {
+    /// The Info.plist
     public static let info = PropertyListManager.Name(rawValue: "Info")
 }
 
+/// Handles caching and decoding of plists.
 public enum PropertyListManager {
     
+    /// A type-safe string value representing the name of a plist to load.
     public struct Name: RawRepresentable {
         public init(rawValue: String) { self.rawValue = rawValue }
         public let rawValue: String
     }
     
+    /// Describes errors thrown by this type.
     public enum Error: Swift.Error { case invalidName }
     
-    public static func objectFromList<T>(named name: Name) throws -> T where T: Decodable {
+    /// Loads the provided plist and converts to type `T`.
+    /// - Parameters:
+    ///   - name: The plist name.
+    ///   - type: The type to decode the plist into.
+    ///   - bundle: The bundle to load the plist from. Defaults to `main`.
+    /// - Returns: The plist decoded to type `T`.
+    public static func objectFromList<T>(named name: Name, ofType type: T.Type, from bundle: Bundle = Bundle.main) throws -> T where T: Decodable {
         // Attempt pulling from cache
         if let cached = plistCache.object(forKey: name.rawValue as NSString)?.value as? T { return cached }
         // Otherwise read data and decode
-        let data = try dataFromList(named: name)
+        let data = try dataFromList(named: name, from: bundle)
         let value = try decoder.decode(T.self, from: data)
         // Store in cache and return
         plistCache.setObject(ValueWrapper(value: value), forKey: name.rawValue as NSString)
         return value
     }
     
-    public static func dictionaryFromList(named name: Name) throws -> [String: Any] {
+    /// Loads the provided plist as a dictionary.
+    /// - Parameters:
+    ///   - name: The plist name.
+    ///   - bundle: The bundle to load the plist from. Defaults to `main`.
+    /// - Returns: The plist decoded as a dictionary.
+    public static func dictionaryFromList(named name: Name, from bundle: Bundle = Bundle.main) throws -> [String: Any] {
         // Attempt pulling from cache
         if let cached = plistCache.object(forKey: name.rawValue as NSString)?.value as? [String: Any] { return cached }
         // Otherwise read data and decode
-        let data = try dataFromList(named: name)
+        let data = try dataFromList(named: name, from: bundle)
         let dictionary = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as! [String: Any]
         // Store in cache and return
         plistCache.setObject(ValueWrapper(value: dictionary), forKey: name.rawValue as NSString)
         return dictionary
     }
     
-    private static func dataFromList(named name: Name) throws -> Data {
-        guard let url = Bundle.main.url(forResource: name.rawValue, withExtension: "plist") else { throw Error.invalidName }
+    private static func dataFromList(named name: Name, from bundle: Bundle) throws -> Data {
+        guard let url = bundle.url(forResource: name.rawValue, withExtension: "plist") else { throw Error.invalidName }
         return try Data(contentsOf: url)
     }
 }
